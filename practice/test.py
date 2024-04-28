d1d1f2fd0569d3bcae0a59e77def0053e5af957d
eromanmat <eromanmat79@gmail.com>
2024-04-28 09:21:27 +0200

    add practice folder

---
+++ practice/test.py
@@ -0,0 +1,32 @@
+
+
+"""   old = int(input("Enter you age: "))
+    car = input("Enter:")
+    old_car = 18
+    if old >= old_car and car == "Da":
+        print('yes')
+    elif old >= old_car and car == 'Net':
+        print ('18 и больше но без опыта')
+    else:
+        print('no')"""
+
+
+# old = int(input("Enter you age: "))
+# car = input("Enter:")
+# old_car = 18
+
+# if old >= old_car and car == "Da":
+#     print('yes')
+# if old >= old_car and car == 'Net':
+#     print('18 и больше, но без опыта')
+# else:
+#     print('no')
+
+
+# def reverse_words(text):
+#     words = text.split()
+#     reversed_words = [word[::-1] for word in words]
+#     reversed_text = " ".join(reversed_words)
+#     print (reversed_text)
+
+# reverse_words("double  spaced  words")
